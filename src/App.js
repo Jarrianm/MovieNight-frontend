d44dda72dc8de35a610471378e1519d4c93b5bd3
Jarrianm <jarrian.mclean@gmail.com>
2019-03-20 04:09:27 -0400

    persistant user login

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import ShowContainer from "./Containers/ShowContainer"
 import { Route, Switch, withRouter } from 'react-router-dom'
 import Login from './Login/Login'
 import ProfileContainter from './Containers/ProfileContainer'
+
+
 class App extends Component {
   state = {
-    user: {}
+    user: {},
+    currentUser:{}
   }
 
   login = (user) => {
@@ -16,7 +19,7 @@ class App extends Component {
       headers: {
         "Accept": "application/json",
         "Content-type": "application/json",
-        "Authorization": localStorage.getItem("token"),
+        "Authorization": localStorage.getItem("token")
       },
     body: JSON.stringify({
         username: user.username,
@@ -28,7 +31,9 @@ class App extends Component {
     .then(data =>{
   
       if (data.user){
-      localStorage.setItem("token", data.jwt);
+        // debugger
+      localStorage.setItem("token", data.token);
+      // debugger
         this.props.history.push('/profile')
         this.setState({
           user: user
@@ -37,18 +42,29 @@ class App extends Component {
       }
     )}
     
-  componentDidMount = () => {
-    if (localStorage.getItem('token') != "undefined"){
-      alert("user logged in")
-          
-
-  }else {
-    // this.login(token)
-    console.log("no user logged in")
-  }
-}
+    // componentDidMount = () => {
+    //   console.log('app mount')
+    //   if (localStorage.getItem('token')){
+    //     alert("user logged in")
+    //     fetch("http://localhost:3000/api/v1/current_user",{
+    //         method: "GET",
+    //        headers: {"Authorization": localStorage.getItem("token")
+    //     }})
+    //     .then(res => res.json())
+    //     .then(data => {
+    //       this.setState({
+    //           currentUser: data
+    //       });
+    //       console.log(this.state.currentUser)
+    //     })
+  
+    // }else {
+    //   // this.login(token)
+    //   console.log("no user logged in")
+    // }
+    // }
   render() {
-    // console.log(this.state)
+    console.log('app render')
     return (
       <div className="App">
      <Route path='/login' render={()=> <Login login={this.login}/>}/>
--- src/Containers/ProfileContainer.js
+++ src/Containers/ProfileContainer.js
@@ -2,14 +2,46 @@ import React, { Component } from 'react';
 
 class ProfileContainer extends Component {
 
-   render() {
+    state = {
+        user: []
+    }
 
-    //   console.log(this)
-      return (
-        <div >
-         <p>Hello {this.props.user.username}</p>
-         
+    componentDidMount = (user) => {
+        console.log('profile mount')
+        if (localStorage.getItem('token')){
+        //   alert("user logged in")
+          fetch("http://localhost:3000/api/v1/current_user",{
+              method: "GET",
+             headers: {"Authorization": localStorage.getItem("token")}})
+          .then(res => res.json())
+          .then(data => {
+            this.setState({user: [...this.state.user, data] })})
+    //else {
+    //     // this.login(token)
+    //     console.log("no user logged in")
+    //   }
+    }}
+   
+    
+
+   render() { 
+    console.log('profile render',this.state.user)
+    let user = this.state.user.map((user) => {
+        console.log(user)
+        return <div key={user.user.id} name={user.user.name}>
+         <p>HI {user.user.username}</p>
         </div>
+    })
+
+
+   
+    return (
+       <div>
+           {user}
+       </div>
+
+
+       
       );
     }
   }
